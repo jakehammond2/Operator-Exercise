57d8e215c5bb3cc47ad57d78cc00c7f6ee4278c2
Jake Hammond <jakehammond226@gmail.com>
2022-07-06 13:17:35 -0500

    Operator Exercise

---
+++ OperatorExercise/OperatorMain.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace OperatorExercise
+{
+    public class OperatorMain
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2; 
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2; 
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2; 
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        
+        public static double AreaOfCirlce(double r)
+        {
+            double radius = Math.PI* Math.Pow(r, 2);
+
+            return radius; 
+        }
+        
+
+
+    }
+
+
+
+
+}
+
+
---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using OperatorExercise;
+
+Console.WriteLine(OperatorMain.Add(5, 2));
+Console.WriteLine(OperatorMain.Subtract(2, 4));
+Console.WriteLine(OperatorMain.Multiply(3, 6));
+Console.WriteLine(OperatorMain.Divide(4, 2));
+Console.WriteLine(OperatorMain.Modulus(5, 2));
+
+
+
+
+Console.WriteLine("Enter in the first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter in the second number");
+int b = Convert.ToInt32(Console.ReadLine());
+int qoutient = a / b;
+int remainder = a % b;
+
+
+Console.WriteLine($"{a}/{b} is {qoutient} remainder {remainder}");
+
+
+
+
+
+Console.WriteLine("What is the radius of your circle?");
+var radius = double.Parse(Console.ReadLine());
+Console.WriteLine($"The area of the circle with radius {radius} is {OperatorMain.AreaOfCirlce(radius)}"); 
+
+
